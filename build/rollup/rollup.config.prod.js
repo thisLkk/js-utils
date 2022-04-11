075c45dfcdcfd3b7ee2c9ae6c8f6dc192433a8e5
lukuankuan <lukuankuan@zhuanzhuan.com>
2022-04-11 13:55:09 +0800

    更新部分代码

--- build/rollup/rollup.config.prod.js
+++ build/rollup/rollup.config.prod.js
@@ -2,7 +2,7 @@ import path from 'path'
 import json from '@rollup/plugin-json' // 解析json文件
 import { babel } from '@rollup/plugin-babel' // 转换es6语法
 import { nodeResolve } from '@rollup/plugin-node-resolve' // 处理node_modeules依赖
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript2';  // 配置ts解析
 import commonjs from '@rollup/plugin-commonjs' // 处理 common 模块js
 import replace from '@rollup/plugin-replace' // 全局替换
 const getPath = _path => path.resolve(__dirname, _path)
@@ -25,14 +25,14 @@ export default {
     }
   ],
   plugins: [
-    typescript({
-      tsconfig: getPath('../../tsconfig.json'), // 导入本地ts配置
-      extensions: ['.js', '.ts']
-    }),
     // 此插件应放在其他插件之前，以便优化代码
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    typescript({
+      tsconfig: getPath('../../tsconfig.json'), // 导入本地ts配置
+      extensions: ['.js', '.ts']
     }),
     json(
       {
